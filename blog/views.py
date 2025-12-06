5aa08b08c5418122b1c8c36932b3ded066ed379c
Felipe César <www.felipecesargomes@gmail.com>
2025-12-06 12:39:36 -0300

    usando context dentro da view

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 def blog(request):
+    context_blog = {
+        'text': 'Bem vindo ao Blog',
+        'range': range(7)
+    }
     print('blog')
     return render(
         request,
-        'blog/index.html', {'range': range(7)}
+        'blog/index.html', 
+        context_blog
     )
 
 def exemplo(request):
+    context_exemplo = {
+        'text': 'Exemplo'
+    }
     print('exemplo')
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context_exemplo
     )
