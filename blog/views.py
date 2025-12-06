24f97b85aab3e764781d49bdfdb0bd8f627d95a6
Felipe César <www.felipecesargomes@gmail.com>
2025-12-06 12:29:48 -0300

    update

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app')
+    return render(
+        request,
+        'blog/index.html', {'range': range(7)}
+    )
 
 def exemplo(request):
     print('exemplo')
-    return HttpResponse('exemplo do app')
+    return render(
+        request,
+        'blog/exemplo.html'
+    )
--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
     print('HOME')
-    return HttpResponse('home do app')
+    return render(
+        request,
+        'home/index.html',
+        {
+            'text': 'Estamos na home'
+        }
+    )
